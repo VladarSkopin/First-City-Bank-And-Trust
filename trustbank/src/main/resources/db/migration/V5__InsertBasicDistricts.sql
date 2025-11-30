0f902b323601517a7dbfa68c64aebb5b5d610083
Alexander Skopintsev <grudagor@gmail.com>
2025-11-30 23:08:00 +0300

    basic districts migration

---
+++ trustbank/src/main/resources/db/migration/V5__InsertBasicDistricts.sql
@@ -0,0 +1,14 @@
+INSERT INTO districts (district_code, district_name) VALUES
+('D-1', 'Dayport'),
+('D-2', 'Wayside Docks'),
+('D-3', 'Eastport'),
+('D-4', 'Olde Quarter'),
+('D-5', 'New Quarter'),
+('D-6', 'South Quarter'),
+('D-7', 'North Quarter'),
+('D-8', 'Stonemarket'),
+('D-9', 'Newmarket'),
+('D-10', 'Auldale'),
+('D-11', 'Downtowne'),
+('D-12', 'Hightowne'),
+('D-13', 'Shalebridge');
