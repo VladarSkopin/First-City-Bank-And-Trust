3a91d054fa036afbf44585968f9219405640eee7
Alexander Skopintsev <grudagor@gmail.com>
2025-12-10 23:53:28 +0300

    basic vaults migration

---
+++ trustbank/src/main/resources/db/migration/V15__InsertBasicVaults.sql
@@ -0,0 +1,9 @@
+insert into vault
+(vault_code, client_code, amount, currency_code, is_archived)
+values
+('VLT-1', 'CLT-00003', 0, 'BBC-123', false),  -- The City Council vault
+('VLT-2', 'CLT-00004', 0, 'BBC-555', false),  -- The City Pension Fund vault
+('VLT-3', 'CLT-00005', 0, 'BBC-123', false),  -- The City Watch Fund vault
+('VLT-4', 'CLT-00007', 0, 'BBC-123', false),  -- The Hammerite Order Fund vault
+('VLT-5', 'CLT-00008', 0, 'BBC-321', false),  -- Wayside Docks Conglomerate vault
+('VLT-6', 'CLT-00010', 0, 'BBC-321', false);  -- The Silver Vault Exchange vault
