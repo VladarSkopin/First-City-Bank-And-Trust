226d662a0ec785134a2f39d736d8b3cd3efd1fdd
Alexander Skopintsev <grudagor@gmail.com>
2025-12-18 23:16:28 +0300

    basic sectors and subsectors migration

--- trustbank/src/main/resources/db/migration/V17__CreateTablesSectors.sql
+++ trustbank/src/main/resources/db/migration/V17__CreateTablesSectors.sql
@@ -9,6 +9,7 @@ COMMENT ON COLUMN sectors.sector_code IS 'Unique sector identifier code.';
 COMMENT ON COLUMN sectors.sector_name IS 'Official sector name for display in interfaces and documentation.';
 COMMENT ON COLUMN sectors.description IS 'Detailed economic characteristics, business activities, and industry drivers.';
 
+
 CREATE TABLE if not exists sub_sectors (
     sub_sector_code TEXT PRIMARY KEY,
     sub_sector_name TEXT NOT NULL UNIQUE,
