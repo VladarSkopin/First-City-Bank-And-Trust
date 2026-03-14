8a1fa7b922cb3957469ee5ca23d43ae1a85cf9b5
Alexander Skopintsev <grudagor@gmail.com>
2026-03-14 09:37:53 +0300

    social ranks info test

--- test-automation/test-frontend/src/test/java/org/skopintsev/social_ranks_page/SocialRankInfoTest.java
+++ test-automation/test-frontend/src/test/java/org/skopintsev/social_ranks_page/SocialRankInfoTest.java
@@ -2,12 +2,15 @@ package org.skopintsev.social_ranks_page;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.skopintsev.assertions.common.elements.ButtonElementAssertions;
+import org.skopintsev.assertions.social_rank.SocialRankCardAssertions;
 import org.skopintsev.assertions.social_rank.SocialRanksPageAssertions;
+import org.skopintsev.models.api.SocialRank;
 import org.skopintsev.transport.PostApiResponseHelper;
 
 import java.util.Collections;
@@ -19,7 +22,9 @@ public class SocialRankInfoTest extends BaseSocialRankTest {
     @Description("Test checks the display of Social Ranks page information.")
     @Severity(SeverityLevel.NORMAL)
     public void socialRanksPageInfoTest() {
-
+        SocialRanksPageAssertions.checkPageTitleText("Social Ranks");
+        SocialRanksPageAssertions.checkSocialRanksCountLabelText("TOTAL RANKS: ");
+        SocialRanksPageAssertions.checkSocialRanksCountValueText(BASE_SOCIAL_RANKS_LIST.size());
         ButtonElementAssertions.checkRetryBtnExists(false);
     }
 
@@ -28,8 +33,18 @@ public class SocialRankInfoTest extends BaseSocialRankTest {
     @Description("Test checks the display of single social rank card information.")
     @Severity(SeverityLevel.NORMAL)
     public void socialRankCardInfoTest() {
+        SocialRank socialRank = BASE_SOCIAL_RANKS_LIST.get(0);
 
-
+        SocialRankCardAssertions.checkSocialRankNameText(socialRank.getRankName());
+        SocialRankCardAssertions.checkSocialRankCodeText(socialRank.getRankCode());
+        SocialRankCardAssertions.checkRankDescriptionText(socialRank.getDescription());
+        SocialRankCardAssertions.checkPrivilegeLabelText("PRIVILEGE LEVEL:");
+        SocialRankCardAssertions.checkPrivilegeValueText(socialRank.getPrivilegeLevel());
+        SocialRankCardAssertions.checkAccessRightsLabelText("ACCESS RIGHTS:");
+        SocialRankCardAssertions.checkAccessRightsValueText(
+                getAccessRightsByPrivilegeLevel(socialRank.getPrivilegeLevel()));
+        SocialRankCardAssertions.checkViewRegulationsBtnEnabled();
+        SocialRankCardAssertions.checkViewRegulationsBtnText("VIEW REGULATIONS");
     }
 
     @Test
@@ -58,6 +73,7 @@ public class SocialRankInfoTest extends BaseSocialRankTest {
         ButtonElementAssertions.checkRetryBtnEnabled(true);
     }
 
+    @Flaky
     @Test
     @Tag("regression")
     @Description("Test checks the display of the Social Ranks page in case of server error response.")
@@ -70,6 +86,19 @@ public class SocialRankInfoTest extends BaseSocialRankTest {
         ButtonElementAssertions.checkRetryBtnExists(true);
         ButtonElementAssertions.checkRetryBtnIsVisible();
         ButtonElementAssertions.checkRetryBtnEnabled(true);
+    }
+
+
+    public String getAccessRightsByPrivilegeLevel(String privilegeLevel) {
+        return switch (privilegeLevel) {
+            case "HIGHEST" -> "UNRESTRICTED";
+            case "HIGH" -> "ELEVATED";
+            case "ELEVATED" -> "MODERATE";
+            case "STANDARD" -> "BASIC";
+            case "RESTRICTED" -> "ESCORTED";
+            default -> "UNKNOWN";
+        };
+
     }
 
 }
